b1b948b82197f13c2cdaaa80b9199e15a7bb7edd
bogdan-necea <necea.bogdan@gmail.com>
2025-05-31 00:45:13 +0300

    v1.0

---
+++ script.js
@@ -0,0 +1,33 @@
+let welcomeScreen=document.querySelectorAll(".skewed");
+let frames=document.querySelectorAll(".frame");
+function dismissWS(){
+    welcomeScreen.forEach(function(ws){
+            ws.style.opacity="0";
+        })
+    setTimeout(function (){
+        welcomeScreen.forEach(function(ws){
+            ws.style.display="none";
+        })
+        frames.forEach(function(f){
+            f.style.display="flex";
+        })
+    },500)
+}
+
+document.addEventListener("keydown",function(event){
+    if(event.key=="Enter"){
+        dismissWS();
+    }
+})
+
+setTimeout(function (){
+    dismissWS();
+},0)
+
+let bChar = document.querySelector("blink-char");
+setInterval(function (){
+    bChar.innerHTML=" ";
+    setTimeout(function(){
+        bChar.innerHTML="▁";
+    },750)
+},1500)
