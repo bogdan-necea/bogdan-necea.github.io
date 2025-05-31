bfaea0d05645c81a308f0ab0bd8a1056242e3f6b
bogdan-necea <necea.bogdan@gmail.com>
2025-06-01 00:54:17 +0300

    v1.6

--- sub-pages/snek/script.js
+++ sub-pages/snek/script.js
@@ -87,7 +87,7 @@ function gameOver(){
     isDead=true;
     document.querySelector(".game-grid").style.display = "none";
     document.querySelector(".death-screen").style.display = "block";
-    document.querySelector(".death-screen").innerHTML += "Score:"+ (length-3);
+    document.querySelector(".death-screen").innerHTML += "Press &lt;Enter&gt; to continue ▁<br>Score:"+ (length-3);
     document.addEventListener("keydown", function(event){
         if(event.key=="Enter"){
             this.location.reload();
